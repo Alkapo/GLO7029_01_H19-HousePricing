08a5bb3032d9ef4f5a98bd5c8e28ccef70cd8639
alkapo <ali_assafiri@hotmail.com>
2019-02-17 19:47:12 -0500

    Commit 4.0 Adjut on categories graphs

--- src/graph.py
+++ src/graph.py
@@ -53,18 +53,24 @@ lb_style = LabelBinarizer()
 lb_results = lb_style.fit_transform(obj_df["Hou"])
 pd.DataFrame(lb_results, columns=lb_style.classes_).head()
 '''
-
-for x in (out.columns):
-    if out[x].dtypes == "object":
-        out[x]= out[x].astype('category')
-        print("|", x ,"|" , out[x].cat.categories)
+for name in (out.columns):
+    if out[name].dtypes == "object":
+        out[name]= out[name].astype('category')
 
 
+# Count plots of categorical features
+cat = out.select_dtypes(include=['category']).columns
+f = pd.melt(out, id_vars=['SalePrice'], value_vars=sorted(cat[1:10]))
 
+g = sns.FacetGrid(f, col='variable', col_wrap=3, sharex=False, sharey=True, size=3)
+g = g.map(sns.boxplot, 'value', 'SalePrice')
+[plt.setp(ax.get_xticklabels(), rotation=90) for ax in g.axes.flat]
+g.fig.tight_layout()
+plt.show()
 
 
 #Le nombre détage affect t-il le prix ?
-#sns.factorplot(x = "1stFlrSF", y = "SalePrice", hue = "HouseStyle", kind = 'violin', col = "HouseStyle", data = out)
-#plt.title("La superficie de la maison en fonction du prix")
-#plt.show()
+# sns.factorplot(x = "1stFlrSF", y = "SalePrice", hue = "HouseStyle", kind = 'violin', col = "HouseStyle", data = out)
+# plt.title("La superficie de la maison en fonction du prix")
+# plt.show()
 
